4ff36326d616b286763d33b33c4dde97479792c9
nov1ce <289836737@qq.com>
2026-01-28 02:08:22 +0800

    update: config

--- AlwaysCompassPlugin.cs
+++ AlwaysCompassPlugin.cs
@@ -3,15 +3,21 @@ using HarmonyLib;
 
 namespace AlwaysCompass;
 
-// TODO - adjust the plugin guid as needed
 [BepInAutoPlugin(id: "io.github.nov1ce-lee.alwayscompass")]
-public partial class AlwaysCompassPlugin : BaseUnityPlugin
-{
-    private void Awake()
+    public partial class AlwaysCompassPlugin : BaseUnityPlugin
     {
-        // Put your initialization logic here
-        Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+        public static AlwaysCompassPlugin Instance { get; private set; }
+        public BepInEx.Configuration.ConfigEntry<bool> IsEnabled { get; private set; }
 
-        new Harmony(Id).PatchAll();
+        private void Awake()
+        {
+            Instance = this;
+            // Configuration
+            IsEnabled = Config.Bind("General", "Enabled", true, "Enable or disable the Always Compass mod");
+
+            // Put your initialization logic here
+            Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+
+            new Harmony(Id).PatchAll();
+        }
     }
-}
